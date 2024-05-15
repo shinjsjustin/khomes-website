67b8de81f0e1207395983bf8bca091efdc168331
shinjsjustin <shinjsjustin@gmail.com>
2024-05-14 21:43:31 -0700

    photos 2

--- khomes-website/src/app/page.tsx
+++ khomes-website/src/app/page.tsx
@@ -1,4 +1,10 @@
 // `app/page.tsx` is the UI for the `/` URL
 export default function Page() {
-  return <h1>Hello, Home page!</h1>
+  return <div>
+  <h1>Gallery</h1>
+  <img src="/images/realestate_outside01.jpg" alt="Outside 01"/>
+  <img src="/images/realestate_outside02.jpg" alt="Outside 02"/>
+  <img src="/images/realestate_outside03.jpg" alt="Outside 03"/>
+  <img src="/images/realestate_outside04.jpg" alt="Outside 04"/>
+</div>
 }
