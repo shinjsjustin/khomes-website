9066b11dacdf9f591692c05dfd4f56f6d3db6f21
shinjsjustin <shinjsjustin@gmail.com>
2024-05-21 11:08:21 -0700

    refactoring

--- khomes-website/src/app/page.tsx
+++ khomes-website/src/app/page.tsx
@@ -1,10 +1,57 @@
-// `app/page.tsx` is the UI for the `/` URL
-export default function Page() {
-  return <div>
-  <h1>Gallery</h1>
-  <img src="/images/realestate_outside01.jpg" alt="Outside 01"/>
-  <img src="/images/realestate_outside02.jpg" alt="Outside 02"/>
-  <img src="/images/realestate_outside03.jpg" alt="Outside 03"/>
-  <img src="/images/realestate_outside04.jpg" alt="Outside 04"/>
-</div>
+import PhotoGrid from './PhotoGrid';
+
+//use client
+export default function Page(){
+
+  return <div className="content">
+    {/* Welcome Section */}
+    <div className="section">
+      <h1 className="text-6xl font-bold text-white">Welcome</h1>
+    </div>
+
+    {/* Information Section */}
+    <div className="section bg-black bg-opacity-70">
+      <PhotoGrid/>
+    </div>
+
+    {/* Additional Paragraph Sections
+    <div className="section bg-black bg-opacity-70">
+      <a className="text-xl text-white"href="/files/House_Rules.docx" download="House_Rules.docx">
+        Download House Rules Word
+      </a>
+      <a className="text-xl text-white"href="/files/House_Rules.pdf" download="House_Rules.pdf">
+        Download House Rules PDF
+      </a>
+      <a className="text-xl text-white"href="/files/Lease_Agreement.docx" download="Lease_Agreement.docx">
+        Download Lease Agreement Word
+      </a>
+      <a className="text-xl text-white"href="/files/Lease_Agreement.pdf" download="Lease_Agreement.pdf">
+        Download Lease Agreement PDF
+      </a>
+    </div> */}
+
+        {/* Additional Paragraph Sections */}
+    <div className="section bg-black bg-opacity-70">
+      <div className = "filebox">
+        <a className="text-xl text-white">
+          House Rules
+        </a>
+        <div className = "icons">
+          <a href="/files/House_Rules.pdf" download="House_Rules.pdf" className="icon" style={{backgroundImage: "url('/icon/pdf.png')" }} />
+          <a href="/files/House_Rules.docx" download="House_Rules.docx" className="icon" style={{backgroundImage: "url('/icon/word.png')"}} />
+        </div>
+      </div>
+      <div className = "filebox">
+        <a className="text-xl text-white">
+          Lease Agreement
+        </a>
+        <div className = "icons">
+          <a href="/files/Lease_Agreement.pdf" download="Lease_Agreement.pdf" className="icon" style={{backgroundImage: "url('/icon/pdf.png')"}} />
+          <a href="/files/Lease_Agreement.docx" download="Lease_Agreement.docx" className="icon" style={{backgroundImage: "url('/icon/word.png')"}} />
+        </div>
+      </div>
+    </div>
+
+    {/* More sections can be added similarly */}
+  </div>
 }
